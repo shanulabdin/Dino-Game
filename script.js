5ecb9c728310c8419ab55e4cee7c4dd0cad3ecb6
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-12 16:52:08 +0500

    made by heart

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-let character = document.getElementById('character');
-let block = document.getElementById('block');
+let dino = document.getElementById('dino');
+let cactus = document.getElementById('cactus');
 
 let isJumping = false;
 
 function jump(){
   if(!isJumping){
     isJumping = true;
-    character.classList.add('animate');
+    dino.classList.add('animate');
+
     
     setTimeout(function(){
-      character.classList.remove('animate');
+      dino.classList.remove('animate');
       isJumping = false;
-    }, 500);
+    }, 500)
   }
 }
 
-let checkDead = setInterval(function(){
-  let characterTop = parseInt(window
-    .getComputedStyle(character)
-    .getPropertyValue('top'));
-  
-  let blockLeft = parseInt(window
-    .getComputedStyle(block)
-    .getPropertyValue('left'));
-  
 
-  if(blockLeft > 50 && blockLeft < 70 && characterTop >= 130){
-    block.style.animation = 'none'
-    alert('u lose');
+
+let isDead = setInterval(function(){
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+  
+  if(cactusLeft > 0 && cactusLeft < 20 && dinoTop >= 130){
+    cactus.style.animation = 'none';
+    alert('You Lose');
   }
-}, 10)
-
+}, 10);
 
 document.addEventListener('click', function(){
   jump();
-});
-
+})
 document.addEventListener('keydown', function(){
   jump();
-});
+})
