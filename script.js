85d662f250c9e564851e336b75c7c140ae9f937f
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-13 16:07:21 +0500

    create the naimation of cactus

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let game = document.getElementById('game');
 let dino = document.getElementById('dino');
-// let cactus = document.querySelector('.cactus');
 
 let isJumping = false;
 let isGameOver = false;
@@ -38,140 +37,25 @@ function isCollision(dino, cactus){
 }
 
 
-setInterval(() => {
-  console.log('collision?', isCollision(dino, cactus))
-}, 1000)
+const cactus = document.createElement('div');
+cactus.className = 'cactus';
+cactus.style.left = game.clientWidth + 'px';
 
+game.appendChild(cactus);
 
+// we set this to minus the amount from the cactus in future
+const speed = 6;
 
+function move() {
+  let left = parseFloat(cactus.style.left);
 
+  // minus the maount of speed from the left of cactus
+  left -= speed;
 
-
-
-
-
-
-
-// function isCollision(dino, cactus){
-  //   const dinoBox = dino.getBoundingClientRect();
-  //   const cactusBox = cactus.getBoundingClientRect();
-
-//   return !(
-//     dinoBox.bottom < cactusBox.top ||
-//     dinoBox.top > cactusBox.bottom ||
-//     dinoBox.right < cactusBox.left ||
-//     dinoBox.left > cactusBox.right
-//   );
-// }
-
-// const GAME_WIDTH = game.clientWidth;
-
-// function createCactus(){
-//   if (isGameOver) return;
-
-//   const cactus = document.createElement('div');
-//   cactus.className = 'cactus';
-//   cactus.style.left = GAME_WIDTH + 'px';
-
-//   game.appendChild(cactus);
-
-//   const speed = 9;
-
-//   function move() {
-//     if (isGameOver) {
-//       if (cactus.parentNode) cactus.parentNode.removeChild(cactus);
-//       return;
-//     }
-//     let left = parseFloat(cactus.style.left);
-//     left -= speed;
-//     cactus.style.left = left + 'px';
-
-//     if (isCollision(dino, cactus)){
-//       gameOver();
-//       return;
-//     }
-
-//     if(left < -50) {
-//       if(cactus.parentNode) cactus.parentNode.removeChild(cactus);
-//       return;
-//     }
-
-//     requestAnimationFrame(move);
-//   }
-//   requestAnimationFrame(move);
-
-//   const min = 500;
-//   const max = 1200;
-//   const delay = Math.random() * (max - min) + min;
-//   setTimeout(createCactus, delay);
-// }
-
-// // createCactus();
-
-
-// const startBtn = document.getElementById('startBtn');
-
-// startBtn.addEventListener('click', () => {
-//   if(isGameOver) return;
-//   createCactus();
-//   startBtn.style.display = 'none';
-// })
-
-// function gameOver() {
-//   isGameOver = true;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function isCollision(dino, cactus){
-
-//   const dinoBox = dino.getBoundingClientRect();
-//   const cactusBox = cactus.getBoundingClientRect();
+  // set the style to cactus
+  cactus.style.left = left + 'px'
   
-//   console.log('dino', dinoBox)
-//   console.log('cactus', cactusBox)
-
-//   return !(
-//     dinoBox.bottom < cactusBox.top ||
-//     dinoBox.top > cactusBox.bottom ||
-//     dinoBox.right < cactusBox.left ||
-//     dinoBox.left > cactusBox.right 
-//   );
-  
-// }
-
-// setInterval(() => {
-//   console.log("Collision?", isCollision(dino, cactus));
-// }, 2000);
-
+  // makes the animation with the frame rate, instead of every milisecond with setInterval()
+  requestAnimationFrame(move)
+}
+move()
